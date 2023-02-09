65d1cd6e29c022f0e7964f5cadc49f9e044e4ff0
abubakri <abubakrilaitan@gmail.com>
2023-02-09 17:10:27 +0100

    first project

--- server.php
+++ server.php
@@ -7,6 +7,7 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+error_reporting(E_ALL);
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
@@ -14,8 +15,8 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
-}
+// if ($uri !== '/' && file_exists('../public_html'.$uri)) {
+//     return false;
+// }
 
-require_once __DIR__.'/public/index.php';
+require_once '../public_html/index.php';
